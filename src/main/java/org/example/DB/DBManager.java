9c075f70de550445d228fa785473b14e8cff9fc4
Nicolas Julian Torres <71136123+JulianTorres1@users.noreply.github.com>
2024-12-12 15:32:56 -0500

    finish

--- src/main/java/org/example/DB/DBManager.java
+++ src/main/java/org/example/DB/DBManager.java
@@ -24,8 +24,87 @@ public class DBManager {
         return false;
     }
 
+    // ----- List records -----
+
     public void listRecords(String tableName) {
         String query = "SELECT * FROM " + tableName;
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                int columnCount = resultSet.getMetaData().getColumnCount();
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(resultSet.getString(i) + "\t");
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+        }
+    }
+
+    // ----- Insert record -----
+
+    public void insertRecord(String tableName, String query) {
+        String fullQuery = "INSERT INTO " + tableName + " " + query;
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(fullQuery);
+            System.out.println("Record inserted successfully into table " + tableName + "!");
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+        }
+    }
+
+    // ----- Update record -----
+    public void updateRecord(String tableName, String query) {
+        String fullQuery = "UPDATE " + tableName + " SET " + query;
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(fullQuery);
+            System.out.println("Record updated successfully in table " + tableName + "!");
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+        }
+    }
+
+    // ----- Delete record -----
+
+    public void deleteRecord(String tableName, String num_bastidor_id) {
+        String query = "DELETE FROM " + tableName + " WHERE num_bastidor = '" + num_bastidor_id + "'";
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+            System.out.println("Record deleted successfully from table " + tableName + "!");
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+        }
+    }
+
+    // --- filter records by price  DESC ---
+
+    public void filterRecordsByPriceDESC(String tableName) {
+        String query = "SELECT * FROM " + tableName + " ORDER BY precio DESC";
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                int columnCount = resultSet.getMetaData().getColumnCount();
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(resultSet.getString(i) + "\t");
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+        }
+    }
+
+    // --- filter records by price ASC ---
+    public void filterRecordsByPriceASC(String tableName) {
+        String query = "SELECT * FROM " + tableName + " ORDER BY precio ASC";
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 import org.example.DB.DBManager;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,7 +19,7 @@ public class Main {
 
         int option = 0;
         while (result) {
-            System.out.println("1. Listar Registros \n2. Insertar Registro \n3. Actualizar Registro \n4. Eliminar Registro \n5. Salir");
+            System.out.println("1. Listar Registros \n2. Insertar Registro \n3. Actualizar Registro \n4. Eliminar Registro \n5. Filtrar por Precio ( Desendente ) \n6. Filtrar por Precio ( Acendente ) \n7. Salir");
             System.out.println("Seleccione una opción: ");
             option = Integer.parseInt(System.console().readLine());
 
@@ -28,15 +29,102 @@ public class Main {
                     System.out.println("test");
                     break;
                 case 2:
-//                    dbManager.insertRecord();
+                    Scanner scanner = new Scanner(System.in);
+
+                    System.out.print("Enter num_bastidor: ");
+                    String numBastidor = scanner.nextLine();
+
+                    System.out.print("Enter nombre_modelo: ");
+                    String nombreModelo = scanner.nextLine();
+
+                    System.out.print("Enter precio: ");
+                    double precio = scanner.nextDouble();
+
+                    System.out.print("Enter descuento: ");
+                    double descuento = scanner.nextDouble();
+
+                    System.out.print("Enter potencia_fiscal: ");
+                    int potenciaFiscal = scanner.nextInt();
+
+                    System.out.print("Enter cilindrada: ");
+                    int cilindrada = scanner.nextInt();
+
+                    System.out.print("Enter en_stock (true/false): ");
+                    boolean enStock = scanner.nextBoolean();
+
+                    System.out.print("Enter id_concesionario: ");
+                    int idConcesionario = scanner.nextInt();
+
+                    System.out.print("Enter id_servicio: ");
+                    int idServicio = scanner.nextInt();
+
+                    scanner.nextLine();
+
+                    System.out.print("Enter fecha_agregado (YYYY-MM-DD format or leave blank for default): ");
+                    String fechaAgregado = scanner.nextLine();
+
+                    String query;
+                    if (fechaAgregado.isEmpty()) {
+                        query = String.format("(num_bastidor, nombre_modelo, precio, descuento, potencia_fiscal, cilindrada, en_stock, id_concesionario, id_servicio) VALUES ('%s', '%s', %.2f, %.2f, %d, %d, %b, %d, %d)",
+                                numBastidor, nombreModelo, precio, descuento, potenciaFiscal, cilindrada, enStock, idConcesionario, idServicio);
+                    } else {
+                        query = String.format("(num_bastidor, nombre_modelo, precio, descuento, potencia_fiscal, cilindrada, en_stock, id_concesionario, id_servicio, fecha_agregado) VALUES ('%s', '%s', %.2f, %.2f, %d, %d, %b, %d, %d, '%s')",
+                                numBastidor, nombreModelo, precio, descuento, potenciaFiscal, cilindrada, enStock, idConcesionario, idServicio, fechaAgregado);
+                    }
+
+                    dbManager.insertRecord("vehiculos", query);
+
                     break;
                 case 3:
-//                    dbManager.updateRecord();
+                    // Update record
+                    Scanner updateScanner = new Scanner(System.in);
+
+                    boolean valid = false;
+
+                    while (valid != true) {
+                        try {
+                            System.out.print("Ingrese el Numero del Bastidor del Vehiculo a modificar: ");
+                            String num_bastidor_id = updateScanner.nextLine();
+
+                            System.out.print("Ingrese el dato a modificar: ");
+                            String updateColumn = updateScanner.nextLine();
+
+                            System.out.print("Enter the new value: ");
+                            String newValue = updateScanner.nextLine();
+
+                            String updateQuery = updateColumn + " = '" + newValue + "' WHERE num_bastidor = '" + num_bastidor_id + "'";
+
+                            dbManager.updateRecord("vehiculos", updateQuery);
+                            valid = true;
+                        } catch (Exception e) {
+                            System.out.println("An error occurred: " + e.getMessage());
+                        }
+                    }
+
+
                     break;
                 case 4:
-//                    dbManager.deleteRecord();
+                    // Delete record
+
+                    try {
+                        Scanner deleteScanner = new Scanner(System.in);
+
+                        System.out.print("Ingrese el Numero del Bastidor del Vehiculo a eliminar: ");
+                        String num_bastidor_id = deleteScanner.nextLine();
+
+                        dbManager.deleteRecord("vehiculos", num_bastidor_id);
+                    } catch (Exception e) {
+                        System.out.println("An error occurred: " + e.getMessage());
+                    }
                     break;
                 case 5:
+                    dbManager.filterRecordsByPriceDESC("vehiculos");
+                    break;
+                case 6:
+                    dbManager.filterRecordsByPriceASC("vehiculos");
+                    break;
+                case 7:
+                    System.out.println("Saliendo...");
                     result = false;
                     break;
                 default:
